671acb276ce436a8fc6351f844751e3c22efff75
James Yang <jy4uk@virginia.edu>
2026-02-03 23:59:27 -0500

    feat:add vercel analytics

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContextNew'
@@ -16,6 +17,7 @@ createRoot(document.getElementById('root')).render(
           <PendingPurchaseProvider>
             <SavedDealsProvider>
               <App />
+              <Analytics />
             </SavedDealsProvider>
           </PendingPurchaseProvider>
         </CartProvider>
